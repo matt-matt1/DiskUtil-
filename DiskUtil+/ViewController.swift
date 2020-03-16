18c4ff7d83c7b5e5c2d9fd905be7b3d189b1e1db
Yuma <yumatechnical@gmail.com>
2020-03-16 15:17:58 -0400

    Initial Commit

---
+++ DiskUtil+/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DiskUtil+
+//
+//  Created by Yuma Technical Inc. on 2020-03-16.
+//  Copyright © 2020 Yuma Technical Inc. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
